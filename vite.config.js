9df659f1f867dd844d2f27520a9e2fe9cdeda798
Husanpreet Singh <167855282+sainihusan@users.noreply.github.com>
2026-04-20 23:08:05 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,15 +6,6 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
-    chunkSizeWarningLimit: 1600,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-        },
-      },
-    },
+    chunkSizeWarningLimit: 2000,
   },
 })
